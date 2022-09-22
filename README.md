9f0c8b283e8bd3fa2d5b967cce9fccc31bca8f33
raaj0505 <raaj0505@gmail.com>
2022-09-22 18:21:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-euuywm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-euuywm)
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  name = 'Angular ' + VERSION.major;
+
+  ngOnInit(): void {
+    document.querySelector('circle').addEventListener('click', (e: any) => {
+      const randomColour = Math.round(Math.random() * 0xffffff);
+      e.target.style.stroke = `#${randomColour.toString(16).padStart(6, '0')}`;
+    });
+  }
+}
